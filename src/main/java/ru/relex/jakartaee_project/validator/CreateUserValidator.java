4258def1419f444b408f0d02f097b9e7bcaac6d9
Notebook <230103108@sdu.edu.kz>
2025-11-26 23:28:51 +0500

    password validator

--- src/main/java/ru/relex/jakartaee_project/validator/CreateUserValidator.java
+++ src/main/java/ru/relex/jakartaee_project/validator/CreateUserValidator.java
@@ -1,17 +1,19 @@
 package ru.relex.jakartaee_project.validator;
 
 import ru.relex.jakartaee_project.dto.CreateUserDto;
-import ru.relex.jakartaee_project.entity.Role;
-import ru.relex.jakartaee_project.entity.User;
 
 import java.util.regex.Pattern;
 
 public class CreateUserValidator implements Validator<CreateUserDto> {
     public static final CreateUserValidator INSTANCE = new CreateUserValidator();
     private static final Pattern EMAIL = Pattern.compile("^[\\w-.]+@[\\w-]+\\.[A-Za-z]{2,}$");
+    private static final Pattern UPPERCASE = Pattern.compile(".*[A-Z].*");
+    private static final Pattern LOWERCASE = Pattern.compile(".*[a-z].*");
+    private static final Pattern DIGIT = Pattern.compile(".*\\d.*");
+    private static final Pattern SPECIAL = Pattern.compile(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*");
 
     private CreateUserValidator() {}
-    public static  CreateUserValidator getInstance() {
+    public static CreateUserValidator getInstance() {
         return INSTANCE;
     }
 
@@ -37,8 +39,22 @@ public class CreateUserValidator implements Validator<CreateUserDto> {
         // password
         if (password == null || password.trim().isEmpty()) {
             result.addError(Error.of("empty.password", "Password is required"));
-        } else if (password.length() < 4) {
-            result.addError(Error.of("weak.password", "Password must be at least 6 characters"));
+        } else {
+            if (password.length() < 6) {
+                result.addError(Error.of("weak.password.length", "Password must be at least 6 characters"));
+            }
+            if (!UPPERCASE.matcher(password).matches()) {
+                result.addError(Error.of("weak.password.uppercase", "Password must contain at least one uppercase letter"));
+            }
+            if (!LOWERCASE.matcher(password).matches()) {
+                result.addError(Error.of("weak.password.lowercase", "Password must contain at least one lowercase letter"));
+            }
+            if (!DIGIT.matcher(password).matches()) {
+                result.addError(Error.of("weak.password.digit", "Password must contain at least one number"));
+            }
+            if (!SPECIAL.matcher(password).matches()) {
+                result.addError(Error.of("weak.password.special", "Password must contain at least one special character"));
+            }
         }
 
         return result;
